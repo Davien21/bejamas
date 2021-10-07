1d1441ee0736d60bc14bfb425972503e2eec5ae9
davien21 <chidiebereekennia@gmail.com>
2021-10-07 15:08:37 +0100

    --feat: add dummy product grid component

---
+++ components/ProductGrid/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./product-grid.module.css";
+
+function ProductGrid(props) {
+  return <div>Hello Product Grid! 😉</div>;
+}
+
+export default ProductGrid;
