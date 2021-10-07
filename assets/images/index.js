7f4a3eaa36fe387fb27b3d5eafe5120b38488db3
davien21 <chidiebereekennia@gmail.com>
2021-10-07 15:04:25 +0100

    --feat: add pngs and svgs

---
+++ assets/images/index.js
@@ -0,0 +1,2 @@
+export * from "./pngs";
+export * from "./svgs";
---
+++ assets/images/pngs/index.js
@@ -0,0 +1,6 @@
+import product1 from "./product-1.png";
+import product2 from "./product-2.png";
+import product3 from "./product-3.png";
+import product4 from "./product-4.png";
+
+export { product1, product2, product3, product4 };
