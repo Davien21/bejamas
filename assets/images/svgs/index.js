26bf5624e254201a73494465dec17ea02ed96307
davien21 <chidiebereekennia@gmail.com>
2021-10-07 15:07:21 +0100

    --feat: add pngs and svgs

---
+++ assets/images/svgs/index.js
@@ -0,0 +1,15 @@
+import CartIcon from "./cart.svg";
+import LogoIcon from "./logo.svg";
+import AscendingIcon from "./ascending.svg";
+import DescendingIcon from "./descending.svg";
+import DropDownIcon from "./drop-down.svg";
+import FilterIcon from "./filter.svg";
+
+export {
+  CartIcon,
+  LogoIcon,
+  AscendingIcon,
+  DescendingIcon,
+  DropDownIcon,
+  FilterIcon,
+};
