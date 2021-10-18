4d92911305926f0a435eb8387b208fbc887c7341
davien21 <chidiebereekennia@gmail.com>
2021-10-18 13:59:08 +0100

    --refactor: adjust to allow any props

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import styles from "./button.module.css";
 
-function Button({ type, text, onClick }) {
+function Button({ type, text, onClick, ...rest }) {
   if (!type) type = "primary";
 
   return (
     <button
+      {...rest}
       onClick={onClick}
       className={`${styles["container"]} ${styles[type]}`}
     >
