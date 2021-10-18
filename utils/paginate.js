d9a6b2a851cd243ab2d7fb7af1d252d37c15cb9f
davien21 <chidiebereekennia@gmail.com>
2021-10-18 14:08:52 +0100

    --feat: remove unneeded files

--- utils/paginate.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-
-function paginate(items, pageNumber, pageSize) {
-  const startIndex = (pageNumber - 1) * pageSize;
-
-  return _(items).slice(startIndex).take(pageSize).value();
-}
-
-export { paginate };
