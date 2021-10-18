29079010d0cb0c7d99a919c37b7e5d668dca28e2
davien21 <chidiebereekennia@gmail.com>
2021-10-18 14:05:16 +0100

    --refactor: use context

--- components/ToggleFilter/index.jsx
+++ components/ToggleFilter/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { AscendingIcon, DescendingIcon } from "../../assets/images";
+import { useSortingContext } from "../../contexts";
 import SelectBox from "../SelectBox";
 
 import styles from "./toggle-filter.module.css";
 
-function ToggleFilter({
-  sortPath,
-  onChangePath,
-  options,
-  sortOrder,
-  onChangeOrder,
-}) {
+function ToggleFilter() {
+  const { sortOrder, setSortOrder, sortPath, setSortPath, orderOptions } =
+    useSortingContext();
+
   let toggleOrderClass = (order) => {
     if (sortOrder !== order) return `${styles["svg-container"]}`;
     return `${styles["svg-container"]} ${styles["active"]}`;
@@ -18,27 +16,29 @@ function ToggleFilter({
   return (
     <div className="flex items-center">
       <div className="flex items-center">
-        <div
-          onClick={() => onChangeOrder("asc")}
+        <button
+          aria-label="Sort in Ascending Order"
+          onClick={() => setSortOrder("asc")}
           className={toggleOrderClass("asc")}
         >
           <AscendingIcon />
-        </div>
+        </button>
 
-        <div
-          onClick={() => onChangeOrder("desc")}
+        <button
+          aria-label="Sort in Ascending Order"
+          onClick={() => setSortOrder("desc")}
           className={toggleOrderClass("desc")}
         >
           <DescendingIcon />
-        </div>
+        </button>
       </div>
       <div className="flex items-center">
         <span className={`${styles["grey-font"]} mx-2`}>Sort By</span>
         <div>
           <SelectBox
             selectedOption={sortPath}
-            onChangeOption={onChangePath}
-            options={options}
+            onChangeOption={setSortPath}
+            options={orderOptions}
           />
         </div>
       </div>
